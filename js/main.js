7f701c198cdd3435927e61a9f9ad0b9349a1d856
kanako <87798077+kanako33333@users.noreply.github.com>
2022-02-11 12:54:06 +0900

    スマホ対応しました。

--- js/main.js
+++ js/main.js
@@ -42,3 +42,15 @@ function delayScrollAnime() {
 		delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
 		scale: 100
 	});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+
+	// スマホ版メニュー
+	$(".openbtn1").click(function () {//ボタンがクリックされたら
+		$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+		$("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+	});
+	
+	$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+		$(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+		$("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+	});
